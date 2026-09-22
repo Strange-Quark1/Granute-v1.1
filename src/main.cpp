cb5d8d1c43ca64792ab2b6d6b48e592011259c0d
Abhinaba Chowdhury <74255289+Strange-Quark1@users.noreply.github.com>
2026-09-23 01:45:47 +0530

    Refactor main function and add UI elements
    Refactor main function to improve readability and add UI for particle manipulation.

--- src/main.cpp
+++ src/main.cpp
@@ -3,173 +3,234 @@
 #include "entities.h"
 #include "quadtree.h"
 #include "physics.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
+int GetClickedParticle(ObjectManager &manager, Vector2 mousePos)
+{
+    for (int i = 0; i < manager.activeObjects; i++)
+    {
+        float dx = manager.posX[i] - mousePos.x;
+        float dy = manager.posY[i] - mousePos.y;
+        if (dx * dx + dy * dy <= manager.radii[i] * manager.radii[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 
-int main() {
-    InitWindow(800, 600, "Custom C++ Physics Engine");
+int main()
+{
+    InitWindow(800, 600, "granute");
     SetTargetFPS(120);
-    
+
     ObjectManager manager;
     QuadTreeAllocator allocator(60000); // pre allocate 10000 quad tree nodes
     std::vector<Point> found;
     found.reserve(2000);
-    //for (int i = 0; i < 4; i++){
-        // manager.AddObject(
-        //     (float)(rand() % 800), // x
-        //     (float)(rand() % 600), // y
-        //     (float)(rand() % 200 - 100) / 1.0f, // vx
-        //     (float)(rand() % 200 - 100) / 1.0f, // vy
-        //     0.0f, // ax
-        //     0.0f, // ay
-        //     1.0f, // mass
-        //     3.0f, // radius
-        //     1//(float)(rand() % 100) / 100.0f  // restitution
-        // );
-    // manager.AddObject(300-100, 200, 0, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
-    // manager.AddObject(400-100, 200, 0, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
-    // manager.AddObject(450-100, 286.602, 0, 0, 0, 400, 1.0f, 30.0f, 1.0f , 0.0f);
-    // manager.AddObject(400-100, 373.204, 0, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
-    // manager.AddObject(300-100, 373.204, 0, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
-    // manager.AddObject(250-100, 286.602, 0, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
 
-    // manager.AddObject(300+150, 200, -400, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
-    // manager.AddObject(400+150, 200, -400, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
-    // manager.AddObject(450+150, 286.602, -400, 0, 400, 0, 1.0f, 30.0f, 1.0f , 1.0f);
-    // manager.AddObject(400+150, 373.204, -400, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
-    // manager.AddObject(300+150, 373.204, -400, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
-    // manager.AddObject(250+150, 286.602, -400, 0, 0, 400, 1.0f, 30.0f, 1.0f, 1.0f);
-    
-    // for (int i = 1 ; i < 19 ; i++){
-    //     manager.AddObject(
-    //         200+i*20.0f, // x
-    //         300.0f, // y
-    //         0.0f, // vx
-    //         0.0f, // vy
-    //         0.0f, // ax
-    //         400.0f, // ay
-    //         3.0f, // mass
-    //         9.0f, // radius
-    //         1.0f,  // restitution
-    //         (i==1 || i ==18)? 0.0f : 1.0f   // free
-    //     );
-    // }
+    Rectangle uiBounds = {10, 10, 180, 230};
+    int currentMode = 0; // 0: spawn, 1: stick
+    float uiRadius = 10.0f;
+    float uiMass = 1.0f;
+    float uiRestitution = 0.5f;
+    bool uiPinned = false;
+    float uiStiffness = 0.05f;
+    int selectedLinkA = -1;
+    int draggedParticle = -1; 
 
-    
-    
-
-
-    for (int i = 0 ; i < 10001; i++){
+    for (int i = 0; i < 5001; i++)
+    {
         manager.AddObject(
-        (float)(rand() % 780 + 10), 
-        (float)(rand() % 580 + 10), 
-        (float)(rand() % 400 - 200) / 1.0f, // vx 
-        (float)(rand() % 400 - 200) / 1.0f, // vy
-        0.0f, // ax
-        0.0f, // ay
-        1.0f, // mass
-        1.0f, // radius
-        1.0f,  // restitution
-        1.0f   // free
-);
+            (float)(rand() % 780 + 10),
+            (float)(rand() % 580 + 10),
+            (float)(rand() % 400 - 200) / 1.0f, // vx
+            (float)(rand() % 400 - 200) / 1.0f, // vy
+            0.0f,                               // ax
+            0.0f,                               // ay
+            1.0f,                               // mass
+            1.0f,                               // radius
+            1.0f,                               // restitution
+            1.0f                                // free
+        );
     }
 
-    while (!WindowShouldClose()) {
-        
-        manager.Update();
-            allocator.reset();
-            Boundary boundary = {400, 300, 400, 300};
-            QuadTree* qt = allocator.allocate(boundary, 32);
-            for (int j = 0; j < manager.activeObjects; j++) {
-                Point p = {j, manager.posX[j], manager.posY[j]};
-                qt->insert(p);
+    while (!WindowShouldClose())
+    {
 
-            }
-        for (int i = 0; i<4; i++)
+        Vector2 mousePos = GetMousePosition();
+        bool overUI = CheckCollisionPointRec(mousePos, uiBounds);
+
+        // Handle Mouse Clicks (only if not clicking the UI panel)
+        if (!overUI && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
-
-            
-            for (int j = 0; j < manager.activeObjects; j++) {
-                Boundary range = {manager.posX[j], manager.posY[j], manager.radii[j]*2, manager.radii[j]*2};
-                found.clear();
-                qt->query(range, found);
-                for (const Point& other : found) {
-                    if (other.id > j) {
-                        collision(manager, j, other.id);
-                        if (j < 18 && other.id < 18) continue;
+            if (currentMode == 0)
+            {
+                // spawn (ay = 400 for gravity)
+                manager.AddObject(mousePos.x, mousePos.y, 0, 0, 0, 400.0f, uiMass, uiRadius, uiRestitution, uiPinned ? 0.0f : 1.0f);
+            }
+            else if (currentMode == 1)
+            {
+                // link
+                int clickedId = GetClickedParticle(manager, mousePos);
+                if (clickedId != -1)
+                {
+                    if (selectedLinkA == -1)
+                    {
+                        selectedLinkA = clickedId; 
                     }
+                    else if (selectedLinkA != clickedId)
+                    {
+                        manager.AddStick(selectedLinkA, clickedId, -1.0f); 
+                        selectedLinkA = -1;
+                    }
+                }
+                else
+                {
+                    selectedLinkA = -1;
                 }
             }
 
-            // stick(manager, 0, 1, 100.0f);
-            // stick(manager, 1, 2, 100.0f);
-            // stick(manager, 2, 3, 100.0f);
-            // stick(manager, 3, 4, 100.0f);
-            // stick(manager, 4, 5, 100.0f);
-            // stick(manager, 5, 0, 100.0f);
-            // stick(manager, 0, 3, 200.0f);
-            // stick(manager, 1, 4, 200.0f);
-            // stick(manager, 2, 5, 200.0f);
-            // stick(manager, 1, 5, 173.205f);
-            // stick(manager, 2, 4, 173.205f);
-
-            // stick(manager, 6, 7, 100.0f);
-            // stick(manager, 7, 8, 100.0f);
-            // stick(manager, 8, 9, 100.0f);
-            // stick(manager, 9, 10, 100.0f);
-            // stick(manager, 10, 11, 100.0f);
-            // stick(manager, 11, 6, 100.0f);
-            // stick(manager, 6, 9, 200.0f);
-            // stick(manager, 7, 10, 200.0f);
-            // stick(manager, 8, 11, 200.0f);
-            // stick(manager, 7, 11, 173.205f);
-            // stick(manager, 8, 10, 173.205f);
-            // for (int i = 0; i < 17; i++){
-            //     stick(manager, i, i+1, 20.0f);
-            // }
-            manager.ApplyConstraints(800.0f, 600.0f);
+            else if (currentMode == 2)
+            {
+               
+                draggedParticle = GetClickedParticle(manager, mousePos);
+            } 
+        } 
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && draggedParticle != -1)
+        {
+            manager.posX[draggedParticle] = mousePos.x;
+            manager.posY[draggedParticle] = mousePos.y;
+            manager.old_posX[draggedParticle] = mousePos.x;
+            manager.old_posY[draggedParticle] = mousePos.y;
         }
 
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+        {
+            draggedParticle = -1; 
+        }
 
+        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+        {
+            int clickedId = GetClickedParticle(manager, mousePos);
+            if (clickedId != -1)
+            {
+                manager.isFree[clickedId] = !manager.isFree[clickedId]; 
+            }
+        }
     
 
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        // Draw the object 
-        // for (int i = 0; i <= 5; i++) {
-        //     Vec2 position(manager.posX[i], manager.posY[i]);
-        //     DrawCircle((int)position.x, (int)position.y, manager.radii[i], BLUE);
-        // }
-        // DrawCircle((int)manager.posX[2], (int)manager.posY[2], manager.radii[2], GREEN);
-        for (int i = 0; i < manager.activeObjects; i++) {
-            Vec2 position(manager.posX[i], manager.posY[i]);
-            DrawCircle((int)position.x, (int)position.y, manager.radii[i], RED);
-        }
-        // DrawLine((int)manager.posX[0], (int)manager.posY[0], (int)manager.posX[1], (int)manager.posY[1], WHITE);
-        // DrawLine((int)manager.posX[1], (int)manager.posY[1], (int)manager.posX[2], (int)manager.posY[2], WHITE);
-        // DrawLine((int)manager.posX[2], (int)manager.posY[2], (int)manager.posX[3], (int)manager.posY[3], WHITE);
-        // DrawLine((int)manager.posX[3], (int)manager.posY[3], (int)manager.posX[4], (int)manager.posY[4], WHITE);
-        // DrawLine((int)manager.posX[4], (int)manager.posY[4], (int)manager.posX[5], (int)manager.posY[5], WHITE);
-        // DrawLine((int)manager.posX[5], (int)manager.posY[5], (int)manager.posX[0], (int)manager.posY[0], WHITE);
-        // DrawLine((int)manager.posX[0], (int)manager.posY[0], (int)manager.posX[3], (int)manager.posY[3], WHITE);
-        // DrawLine((int)manager.posX[1], (int)manager.posY[1], (int)manager.posX[4], (int)manager.posY[4], WHITE);
-        // DrawLine((int)manager.posX[2], (int)manager.posY[2], (int)manager.posX[5], (int)manager.posY[5], WHITE);
-
-        // DrawLine((int)manager.posX[6], (int)manager.posY[6], (int)manager.posX[7], (int)manager.posY[7], WHITE);
-        // DrawLine((int)manager.posX[7], (int)manager.posY[7], (int)manager.posX[8], (int)manager.posY[8], WHITE);
-        // DrawLine((int)manager.posX[8], (int)manager.posY[8], (int)manager.posX[9], (int)manager.posY[9], WHITE);
-        // DrawLine((int)manager.posX[9], (int)manager.posY[9], (int)manager.posX[10], (int)manager.posY[10], WHITE);
-        // DrawLine((int)manager.posX[10], (int)manager.posY[10], (int)manager.posX[11], (int)manager.posY[11], WHITE);
-        // DrawLine((int)manager.posX[11], (int)manager.posY[11], (int)manager.posX[6], (int)manager.posY[6], WHITE);
-        // DrawLine((int)manager.posX[6], (int)manager.posY[6], (int)manager.posX[9], (int)manager.posY[9], WHITE);
-        // DrawLine((int)manager.posX[7], (int)manager.posY[7], (int)manager.posX[10], (int)manager.posY[10], WHITE);
-        // DrawLine((int)manager.posX[8], (int)manager.posY[8], (int)manager.posX[11], (int)manager.posY[11], WHITE);
-
-
-        DrawFPS(10, 30);
-        EndDrawing();
+    manager.Update();
+    allocator.reset();
+    Boundary boundary = {400, 300, 400, 300};
+    QuadTree *qt = allocator.allocate(boundary, 32);
+    for (int j = 0; j < manager.activeObjects; j++)
+    {
+        Point p = {j, manager.posX[j], manager.posY[j]};
+        qt->insert(p);
     }
-    
-    CloseWindow();
-    return 0;
+    for (int i = 0; i < 4; i++)
+    {
+
+        for (int j = 0; j < manager.activeObjects; j++)
+        {
+            Boundary range = {manager.posX[j], manager.posY[j], manager.radii[j] * 2, manager.radii[j] * 2};
+            found.clear();
+            qt->query(range, found);
+            for (const Point &other : found)
+            {
+                if (other.id > j)
+                {
+                    collision(manager, j, other.id);
+                    if (j < 18 && other.id < 18)
+                        continue;
+                }
+            }
+        }
+        for (int s = 0; s < manager.activeSticks; s++)
+        {
+            stick(manager, manager.stickA[s], manager.stickB[s], manager.stickLength[s], uiStiffness);
+        }
+
+        manager.ApplyConstraints(800.0f, 600.0f);
+    }
+
+    BeginDrawing();
+    ClearBackground(BLACK);
+    for (int s = 0; s < manager.activeSticks; s++)
+    {
+        DrawLineEx(
+            (Vector2){manager.posX[manager.stickA[s]], manager.posY[manager.stickA[s]]},
+            (Vector2){manager.posX[manager.stickB[s]], manager.posY[manager.stickB[s]]},
+            2.0f, LIGHTGRAY);
+    }
+
+    for (int i = 0; i < manager.activeObjects; i++)
+    {
+        Vec2 position(manager.posX[i], manager.posY[i]);
+        // pinned objects green, free objects red
+        Color col = manager.isFree[i] == 0.0f ? GREEN : RED;
+        DrawCircle((int)position.x, (int)position.y, manager.radii[i], col);
+    }
+
+    if (currentMode == 1 && selectedLinkA != -1)
+    {
+        DrawCircleLines((int)manager.posX[selectedLinkA], (int)manager.posY[selectedLinkA], manager.radii[selectedLinkA] + 4, YELLOW);
+        DrawLine((int)manager.posX[selectedLinkA], (int)manager.posY[selectedLinkA], mousePos.x, mousePos.y, YELLOW); // Draw string to mouse
+    }
+
+    GuiPanel(uiBounds, "granute");
+
+    // Mode Switcher
+    GuiToggleGroup((Rectangle){15, 35, 50, 20}, "SPAWN;LINK;DRAG", &currentMode);
+
+    if (currentMode == 0)
+    {
+        // Tighter vertical spacing and thinner sliders
+        GuiLabel((Rectangle){15, 65, 50, 15}, "Radius:");
+        GuiSliderBar((Rectangle){65, 65, 110, 15}, "", TextFormat("%.1f", uiRadius), &uiRadius, 2.0f, 40.0f);
+
+        GuiLabel((Rectangle){15, 90, 50, 15}, "Mass:");
+        GuiSliderBar((Rectangle){65, 90, 110, 15}, "", TextFormat("%.1f", uiMass), &uiMass, 0.1f, 10.0f);
+
+        GuiLabel((Rectangle){15, 115, 50, 15}, "Bounce:");
+        GuiSliderBar((Rectangle){65, 115, 110, 15}, "", TextFormat("%.2f", uiRestitution), &uiRestitution, 0.0f, 1.0f);
+
+        GuiCheckBox((Rectangle){15, 140, 15, 15}, "Pinned", &uiPinned);
+    }
+    else if (currentMode == 1)
+    {
+        GuiLabel((Rectangle){15, 65, 150, 15}, selectedLinkA != -1 ? "Select second particle." : "Select first particle.");
+        GuiLabel((Rectangle){15, 90, 60, 15}, "Stiffness:");
+        GuiSliderBar((Rectangle){75, 90, 100, 15}, "", TextFormat("%.2f", uiStiffness), &uiStiffness, 0.01f, 1.0f);
+    }
+
+    else if (currentMode == 2)
+    {
+        GuiLabel((Rectangle){15, 65, 150, 15}, "Drag particles with mouse.");
+    }
+
+    // Tighter buttons at the bottom
+    if (GuiButton((Rectangle){15, 170, 160, 20}, "Clear Screen"))
+    {
+        manager.activeObjects = 0;
+        manager.activeSticks = 0;
+        selectedLinkA = -1;
+    }
+
+    if (GuiButton((Rectangle){15, 195, 160, 20}, "Drop Cluster (x50)"))
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            manager.AddObject(GetRandomValue(300, 500), GetRandomValue(50, 150), 0, 0, 0, 400.0f, uiMass, uiRadius, uiRestitution, 1.0f);
+        }
+    }
+
+    DrawFPS(10, 30);
+    EndDrawing();
+}
+
+CloseWindow();
+return 0;
 }
